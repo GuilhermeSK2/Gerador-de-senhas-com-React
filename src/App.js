1e7801b4ad3a63af0200da782c1fcad6a879f998
GuilhermeSK2 <iosfantasma08@gmail.com>
2024-05-22 16:32:28 -0300

    FIrst Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function GeradorDeSenha() {
+  const [sliderValue, setSliderValue] = useState(15);
+  const [generatedPassword, setGeneratedPassword] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+
+  const generatePassword = () => {
+    let pass = '';
+    for (let i = 0, n = charset.length; i < sliderValue; ++i) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    setGeneratedPassword(pass);
+    setIsPasswordVisible(true);
+  };
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(generatedPassword);
+    alert("Senha copiada com sucesso!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <img className="logo" src='../logo.png' alt="Gerador senha logo" />
+
+      <div className="container-input">
+        <span>Tamanho <span id="valor">{sliderValue}</span> caracteres</span>
+        <input
+          id="slider"
+          className="slider"
+          type="range"
+          min="5"
+          max="25"
+          value={sliderValue}
+          onChange={(e) => setSliderValue(e.target.value)}
+        />
+
+        <button id="button" className="button-cta" onClick={generatePassword}>
+          Gerar senha
+        </button>
+      </div>
+
+      {isPasswordVisible && (
+        <div
+          id="container-password"
+          className="container-password"
+          onClick={copyPassword}
         >
-          Learn React
-        </a>
-      </header>
+          <span className="title">Sua senha gerada foi:</span>
+          <span id="password" className="password">{generatedPassword}</span>
+          <span className="tooltip">Clique na senha para copiar. 👆</span>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default GeradorDeSenha;
